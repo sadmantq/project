c9d5c81666d89a49ef25b4b9bedd1e1acd67160d
sadmantq <sadmancltreq@gmail.com>
2024-03-03 00:50:06 +0600

    mdsd

--- client/src/components/MovieDetails.js
+++ client/src/components/MovieDetails.js
@@ -71,6 +71,7 @@ export default function MovieDetails() {
         {   try
             {
                 const responseFinal = await axios.post(`http://localhost:5000/like/${id}/${userId}`);
+                setLikeClicked(!likeClicked);
             }
             catch(err)
             {
@@ -87,6 +88,7 @@ export default function MovieDetails() {
             try
             {
                 const responseFinal = await axios.delete(`http://localhost:5000/removeLike/${id}/${userId}`);
+                setLikeClicked(!likeClicked);
             }
             catch(err)
             {
@@ -101,6 +103,7 @@ export default function MovieDetails() {
             try
             {
                 const responseFinal = await axios.post(`http://localhost:5000/dislike/${id}/${userId}`);
+                setDislikeClicked(!dislikeClicked);
             }
             catch(err)
             {
@@ -115,6 +118,37 @@ export default function MovieDetails() {
             try
             {
                 const responseFinal = await axios.delete(`http://localhost:5000/removeDislike/${id}/${userId}`);
+                setDislikeClicked(!dislikeClicked);
+            }
+            catch(err)
+            {
+                console.log(err);
+            }
+        }
+    }
+
+    async function handleWatchlist()
+    {
+        {
+            try
+            {
+                const responseFinal = await axios.post(`http://localhost:5000/addToWatchlist/${id}/${userId}`);
+                setWatchlistClicked(!watchlistClicked);
+            }
+            catch(err)
+            {
+                console.log(err);
+            }
+        }
+    }
+
+    async function handleUnwatchlist()
+    {
+        {
+            try
+            {
+                const responseFinal = await axios.delete(`http://localhost:5000/removeFromWatchlist/${userId}/${id}`);
+                setWatchlistClicked(!watchlistClicked);
             }
             catch(err)
             {
@@ -147,12 +181,16 @@ export default function MovieDetails() {
                         if (likeClicked == false)
                         {
                             handleLike();
+                            if (dislikeClicked)
+                            {
+                                handleUndislike();
+                            }
                         }
                         else
                         {
                             handleUnlike();
                         }    
-                        setLikeClicked(!likeClicked)
+                        // setLikeClicked(!likeClicked)
                         //handleLike();
 
                     }}>
@@ -166,12 +204,18 @@ export default function MovieDetails() {
                         if (!dislikeClicked)
                         {
                             handleDislike();
+                            if (likeClicked)
+                            {
+                                handleUnlike();
+                            }
+                            // handleUnlike();
                         }
                         else
                         {
                             handleUndislike();
+                            
                         }
-                        setDislikeClicked(!dislikeClicked);
+                        // setDislikeClicked(!dislikeClicked);
                     }}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-hand-thumbs-down-fill" viewBox="0 0 16 16">
                             <path d="M6.956 14.534c.065.936.952 1.659 1.908 1.42l.261-.065a1.38 1.38 0 0 0 1.012-.965c.22-.816.533-2.512.062-4.51q.205.03.443.051c.713.065 1.669.071 2.516-.211.518-.173.994-.68 1.2-1.272a1.9 1.9 0 0 0-.234-1.734c.058-.118.103-.242.138-.362.077-.27.113-.568.113-.856 0-.29-.036-.586-.113-.857a2 2 0 0 0-.16-.403c.169-.387.107-.82-.003-1.149a3.2 3.2 0 0 0-.488-.9c.054-.153.076-.313.076-.465a1.86 1.86 0 0 0-.253-.912C13.1.757 12.437.28 11.5.28H8c-.605 0-1.07.08-1.466.217a4.8 4.8 0 0 0-.97.485l-.048.029c-.504.308-.999.61-2.068.723C2.682 1.815 2 2.434 2 3.279v4c0 .851.685 1.433 1.357 1.616.849.232 1.574.787 2.132 1.41.56.626.914 1.28 1.039 1.638.199.575.356 1.54.428 2.591"/>
@@ -179,7 +223,17 @@ export default function MovieDetails() {
                     </button>
                 </div>
                 <div className="watch-button">
-                    <button type="button" className={`btn ${watchlistClicked? `btn-warning`: `btn-secondary`}`} onClick={() => setWatchlistClicked(!watchlistClicked)}>
+                    <button type="button" className={`btn ${watchlistClicked? `btn-warning`: `btn-secondary`}`} onClick={() => {
+                        if (!watchlistClicked)
+                        {
+                            handleWatchlist();
+                        }
+                        else
+                        {
+                            handleUnwatchlist();
+                        }
+                        // setWatchlistClicked(!watchlistClicked);
+                    }}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-plus-square-fill" viewBox="0 0 16 16">
                             <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0"/>
                         </svg>
