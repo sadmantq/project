c566d5f1121e0195296f837cffa364f0a7f53e27
sadmantq <sadmancltreq@gmail.com>
2024-03-03 21:13:05 +0600

    ektu vul chilo

--- client/src/admin/AdminMain.js
+++ client/src/admin/AdminMain.js
@@ -1,13 +1,14 @@
 import React from "react";
 import axios from "axios";
-import { Navigate, useNavigate } from "react-router-dom";
+import {  useNavigate } from "react-router-dom";
 
 export default function AdminMain()
 {
+    const navigate = useNavigate();
     return(
         <div className="container">
-            <button type="button" className="btn btn-primary" onClick={()=> Navigate('/admin/addMovie')}>Add a movie</button>
-            <button type="button" className="btn btn-primary" onClick={()=> Navigate('/admin/removeMovie')}>Remove a movie</button>
+            <button type="button" className="btn btn-primary" onClick={()=> navigate('/admin/addMovie')}>Add a movie</button>
+            <button type="button" className="btn btn-primary" onClick={()=> navigate('/admin/removeMovie')}>Remove a movie</button>
         </div>
     )
 }
