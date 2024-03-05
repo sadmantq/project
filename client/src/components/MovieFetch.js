0b7e7582a68fb0f6a9c2fae38f1e5e5935eab317
sadmantq <sadmancltreq@gmail.com>
2024-03-06 03:53:08 +0600

    fagination added(amr frend fagin) :3

--- client/src/components/MovieFetch.js
+++ client/src/components/MovieFetch.js
@@ -41,60 +41,116 @@ import axios from "axios";
 import './MovieFetch.css';
 import { Link, useNavigate } from "react-router-dom";
 import LoginContext from "../context/LoginContext";
-import { useContext } from "react";
+import { useContext, useState,useEffect } from "react";
 import CurrentMovieContext from "../context/CurrentMovieContext";
 import SearchBar from "../Search/SearchBar";
 
 
 export default function MovieFetch(){
 
-    const {setCurrentMovie} = useContext(CurrentMovieContext);
+    // const {setCurrentMovie} = useContext(CurrentMovieContext);
 
     
+    // const navigate = useNavigate();
+
+    // const {loginInfo} = useContext(LoginContext);
+    
+    // const [data, setData] = React.useState([]);
+
+    // React.useEffect(() => {
+    //      axios.get("http://localhost:5000/movies")
+    //         .then(res => setData(res.data));
+    // }, []);
+
+   
+
+    // return (
+    //     <>
+        
+    //     <div className="movie-container">
+    //         <SearchBar   />
+    //         {data.map(item => {
+    //             return (
+    //                 <>
+                    
+    //                 <div className="movie-item" key={item.id} onClick={()=>{
+    //                     setCurrentMovie(item.id);
+    //                     navigate(`/movies/${item.id}`)}}>
+    //                     <h6 className="movie-id">ID: {item.id}</h6>
+    //                     <p className="movie-name">{item.name}</p>
+    //                     <p className="movie-year">Year of Release: {item.year}</p>
+    //                     <img className="movie-image" src={item.image} alt="Movie Poster"/>
+                        
+    //                 </div>
+    //                 </>
+    //             );
+    //         })}
+    //     </div>
+    //     </>
+    // );
+
+    const { setCurrentMovie } = useContext(CurrentMovieContext);
+    const { loginInfo } = useContext(LoginContext);
     const navigate = useNavigate();
+    const [data, setData] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage] = useState(4); // You can change this value as needed
 
-    const {loginInfo} = useContext(LoginContext);
-    
-    const [data, setData] = React.useState([]);
-
-    React.useEffect(() => {
-         axios.get("http://localhost:5000/movies")
+    useEffect(() => {
+        axios.get("http://localhost:5000/movies")
             .then(res => setData(res.data));
     }, []);
 
-    // const handleLogin = async(e) => {
-    //     e.preventDefault();
-    //     // Add your login logic here
-    //     navigate('/')
+    // Logic to get current items based on pagination
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
 
-    
-    // };
+    // Change page
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
     return (
         <>
-        
-        <div className="movie-container">
-            <SearchBar   />
-            {data.map(item => {
-                return (
-                    <>
-                    {/*<p>The username is : {loginInfo}</p>*/}
-                    <div className="movie-item" key={item.id} onClick={()=>{
+            <div className="movie-container">
+                <SearchBar />
+                {currentItems.map(item => (
+                    <div className="movie-item" key={item.id} onClick={() => {
                         setCurrentMovie(item.id);
-                        navigate(`/movies/${item.id}`)}}>
+                        navigate(`/movies/${item.id}`);
+                    }}>
                         <h6 className="movie-id">ID: {item.id}</h6>
                         <p className="movie-name">{item.name}</p>
                         <p className="movie-year">Year of Release: {item.year}</p>
-                        <img className="movie-image" src={item.image} alt="Movie Poster"/>
-                        {/* <p className="movie-adult">{item.is_adult ? 'Adult' : 'Not Adult'}</p>
-                        <p className="movie-genre">Genre: {item.genre}</p>
-                        <p className="movie-description">Review: {item.description}</p> */}
-                        {/*<Link to ={`/movies/${item.id}`}>Click to see full movie</Link>*/}
+                        <img className="movie-image" src={item.image} alt="Movie Poster" />
                     </div>
-                    </>
-                );
-            })}
-        </div>
+                ))}
+            </div>
+            {/* <nav>
+                <ul className="pagination">
+                    {data.length > 0 && Array.from({ length: Math.ceil(data.length / itemsPerPage) }, (_, index) => (
+                        <li key={index} className={`page-item ${currentPage === index + 1 ? 'active' : ''}`}>
+                            <button className="page-link" onClick={() => paginate(index + 1)}>{index + 1}</button>
+                        </li>
+                    ))}
+                </ul>
+            </nav> */}
+
+<nav>
+    <ul className="pagination">
+        <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
+            <button className="page-link" onClick={() => paginate(currentPage - 1)}>Previous</button>
+        </li>
+        {data.length > 0 && Array.from({ length: Math.ceil(data.length / itemsPerPage) }, (_, index) => (
+            <li key={index} className={`page-item ${currentPage === index + 1 ? 'active' : ''}`}>
+                <button className="page-link" onClick={() => paginate(index + 1)}>{index + 1}</button>
+            </li>
+        ))}
+        <li className={`page-item ${currentPage === Math.ceil(data.length / itemsPerPage) ? 'disabled' : ''}`}>
+            <button className="page-link" onClick={() => paginate(currentPage + 1)}>Next</button>
+        </li>
+    </ul>
+</nav>
+
         </>
     );
 }
